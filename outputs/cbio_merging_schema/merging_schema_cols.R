71e95cd1d5a6866745b2850079ec57241adaab12
Sehyun Oh <shbrief@hotmail.com>
2024-05-08 17:30:29 -0400

    update the file paths

--- outputs/cbio_merging_schema/merging_schema_cols.R
+++ outputs/cbio_merging_schema/merging_schema_cols.R
@@ -1,9 +1,9 @@
 ## Analysis-ready formatting ----------------------
 library(OmicsMLRepoCuration)
-ms_fpath <- "~/OmicsMLRepo/OmicsMLRepoData/inst/extdata/cBioPortal_merging_schema.csv"
+ms_fpath <- "data/cBioPortal_merging_schema.csv"
 ms <- readr::read_csv(ms_fpath)
 cols <- c("curated_field", "original_field_num", "original_field")
 ms_long <- OmicsMLRepoR::getLongMetaTb(ms[cols],
                                        targetCols = c("original_field"), 
                                        delim = ";")
-readr::write_csv(x, "~/OmicsMLRepo/OmicsMLRepoHarmonizer/data/cbio_merging_schema/ms_cols.csv")
+readr::write_csv(x, "outputs/cbio_merging_schema/merging_schema_cols.csv")
