e6e3185f950cd78dd27d22dee00b415586a7f468
Sehyun Oh <shbrief@hotmail.com>
2024-11-15 13:31:27 -0500

    delete out-dated files

--- outputs/cbio_merging_schema/merging_schema_cols.R
+++
@@ -1,9 +0,0 @@
-## Analysis-ready formatting ----------------------
-library(OmicsMLRepoCuration)
-ms_fpath <- "data/cBioPortal_merging_schema.csv"
-ms <- readr::read_csv(ms_fpath)
-cols <- c("curated_field", "original_field_num", "original_field")
-ms_long <- OmicsMLRepoR::getLongMetaTb(ms[cols],
-                                       targetCols = c("original_field"), 
-                                       delim = ";")
-readr::write_csv(x, "outputs/cbio_merging_schema/merging_schema_cols.csv")
